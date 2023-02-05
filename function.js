78f8e5ef474716df1cb96bc11b8e25f7617523a1
fitsaleem <sherohero33@gmail.com>
2023-02-05 15:33:17 +0500

    piaic function assignment

---
+++ function.js
@@ -0,0 +1,31 @@
+// using arrow function
+
+console.log("using arrow function")
+let sum3= (a,b)=>{
+    let c=a+b
+    return c
+}
+
+console.log(sum3(3,4))
+console.log(sum3(7,4))
+
+// using annonymous  function
+console.log("using annonymous function")
+
+let sum4= function(a,b){
+    let c=a+b
+    return c
+}
+
+console.log(sum4(3,4))
+console.log(sum4(7,4))
+
+// using function 
+console.log("usng function")
+function sum(a,b){
+    let c=a+b
+    return c
+}
+
+console.log(sum(3,4))
+console.log(sum(7,4))
